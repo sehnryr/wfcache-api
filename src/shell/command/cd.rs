d59e5c848ca0a8ec99904d26fb3c619656c82b64
sehnryr <youn@melois.dev>
2023-02-25 02:34:18 +0100

    chore: update code to match new dependencies version

--- src/shell/command/cd.rs
+++ src/shell/command/cd.rs
@@ -14,7 +14,7 @@ pub fn command(state: &mut State, args: Arguments) -> Result<(), Box<dyn std::er
     let directory = normalize_path(&args.directory, &state.current_lotus_dir);
 
     // Get the directory node
-    let dir_node = state.h_cache.get_dir_node(directory.to_str().unwrap());
+    let dir_node = state.h_cache.get_directory_node(directory.to_str().unwrap());
 
     // Check if the directory exists
     if dir_node.is_none() {
