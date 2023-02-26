sehnryr/wfcache-api

commit b22712dd5bec051ee1bb72a4a682a9d71fad8048

feat: add support for BC6H and BC7 compression formats

src/utils/header/image.rs
@@ -1,17 +1,19 @@
-use ddsfile::{FourCC, Header as DDSHeader, PixelFormatFlags};
+use ddsfile::{DxgiFormat, FourCC, Header as DDSHeader};
 use derivative::Derivative;
 use std::cmp::max;
 
 use crate::utils::header::Header;
 
-fn map_fourcc(value: u8) -> FourCC {
+fn map_dxgi(value: u8) -> DxgiFormat {
     match value {
-        0x00 | 0x01 => FourCC(FourCC::DXT1),
-        0x02 => FourCC(FourCC::DXT3),
-        0x03 => FourCC(FourCC::DXT5),
-        0x06 => FourCC(FourCC::ATI1),
-        0x07 => FourCC(FourCC::ATI2),
-        _ => FourCC(FourCC::NONE),
+        0x00 | 0x01 => DxgiFormat::BC1_UNorm,
+        0x02 => DxgiFormat::BC2_UNorm,
+        0x03 => DxgiFormat::BC3_UNorm,
+        0x06 => DxgiFormat::BC4_UNorm,
+        0x07 => DxgiFormat::BC5_UNorm,
+        0x22 => DxgiFormat::BC7_UNorm,
+        0x23 => DxgiFormat::BC6H_UF16,
+        _ => DxgiFormat::Unknown,
     }
 }
 
@@ -33,20 +35,18 @@ impl Image {
         let data = data[header.size..].to_vec();
         let mut data_offset = 0;
 
-        let mut image = Image::default();
-
         // Skip unknown byte
         data_offset += 1;
 
         // Read the image count
-        image.f_cache_image_count = data[data_offset];
+        let f_cache_image_count: u8 = data[data_offset];
         data_offset += 1;
 
         // Skip unknown byte
         data_offset += 1;
 
         // Read the DDS compression format
-        image.header.spf.fourcc = Some(map_fourcc(data[data_offset]));
+        let format = map_dxgi(data[data_offset]);
         data_offset += 1;
 
         // Read the mip map count
@@ -59,8 +59,9 @@ impl Image {
         data_offset += 4;
 
         // Read the F cache image offsets
+        let mut f_cache_image_offsets = Vec::new();
         for _ in 0..mip_map_count {
-            image.f_cache_image_offsets.push(u32::from_le_bytes([
+            f_cache_image_offsets.push(u32::from_le_bytes([
                 data[data_offset],
                 data[data_offset + 1],
                 data[data_offset + 2],
@@ -93,18 +94,21 @@ impl Image {
         // data_offset += 4;
 
         // Calculate the width and height
+        let width: u32;
+        let height: u32;
         if width_ratio > height_ratio {
-            image.header.width = max_side_length;
-            image.header.height = max_side_length * height_ratio as u32 / width_ratio as u32;
+            width = max_side_length;
+            height = max_side_length * height_ratio as u32 / width_ratio as u32;
         } else {
-            image.header.width = max_side_length * width_ratio as u32 / height_ratio as u32;
-            image.header.height = max_side_length;
+            width = max_side_length * width_ratio as u32 / height_ratio as u32;
+            height = max_side_length;
         }
 
-        // Set the pixel format flags
-        image.header.spf.flags = PixelFormatFlags::FOURCC;
-
-        image
+        Image::new(
+            DDSHeader::new_dxgi(height, width, None, format, None, None, None).unwrap(),
+            f_cache_image_count,
+            f_cache_image_offsets,
+        )
     }
 
     pub fn size(&self) -> usize {
@@ -112,18 +116,22 @@ impl Image {
             * max(1, (self.header.height + 3) as usize / 4 as usize)
             * match self.header.spf.fourcc.clone().unwrap().0 {
                 FourCC::DXT1 | FourCC::ATI1 => 8,
-                FourCC::DXT3 | FourCC::DXT5 | FourCC::ATI2 => 16,
+                FourCC::DXT3 | FourCC::DXT5 | FourCC::ATI2 | FourCC::DX10 => 16,
                 _ => 8,
             }
     }
 }
 
-impl Default for Image {
-    fn default() -> Self {
+impl Image {
+    pub fn new(
+        header: DDSHeader,
+        f_cache_image_count: u8,
+        f_cache_image_offsets: Vec<u32>,
+    ) -> Self {
         Self {
-            header: DDSHeader::default(),
-            f_cache_image_count: 0,
-            f_cache_image_offsets: Vec::new(),
+            header,
+            f_cache_image_count,
+            f_cache_image_offsets,
         }
     }
 }

commit 74278e761527414783fd04afd115a399398f73ee

fix: commit b22712dd broke formats other than DX10

src/shell/command/get.rs
@@ -116,6 +116,7 @@ fn extract_file(
         debug!("Cache offset: {}", _file_node.cache_offset() as u64);
         debug!("Cache image size: {}", _file_node.comp_len() as u64);
         debug!("Real image size: {}", header.size() as u64);
+        debug!("Decompressed image size: {}", _file_node.len() as u64);
 
         let f_cache_offsets = header.f_cache_image_offsets.clone();
 
@@ -152,6 +153,7 @@ fn extract_file(
         debug!("Cache offset: {}", _file_node.cache_offset() as u64);
         debug!("Cache image size: {}", _file_node.comp_len() as u64);
         debug!("Real image size: {}", header.size() as u64);
+        debug!("Decompressed image size: {}", _file_node.len() as u64);
 
         let _file_data = b_cache.decompress_data(file_node.clone())?;
         file_data = _file_data[_file_data.len() - header.size().._file_data.len()].to_vec();

src/utils/header/image.rs
@@ -17,6 +17,17 @@ fn map_dxgi(value: u8) -> DxgiFormat {
     }
 }
 
+fn map_fourcc(value: u8) -> FourCC {
+    match value {
+        0x00 | 0x01 => FourCC(FourCC::DXT1),
+        0x02 => FourCC(FourCC::DXT3),
+        0x03 => FourCC(FourCC::DXT5),
+        0x06 => FourCC(FourCC::ATI1),
+        0x07 => FourCC(FourCC::ATI2),
+        _ => FourCC(FourCC::NONE),
+    }
+}
+
 #[derive(Derivative)]
 #[derivative(Debug)]
 pub struct Image {
@@ -46,6 +57,7 @@ impl Image {
         data_offset += 1;
 
         // Read the DDS compression format
+        let fourcc = map_fourcc(data[data_offset]);
         let format = map_dxgi(data[data_offset]);
         data_offset += 1;
 
@@ -104,11 +116,19 @@ impl Image {
             height = max_side_length;
         }
 
-        Image::new(
-            DDSHeader::new_dxgi(height, width, None, format, None, None, None).unwrap(),
-            f_cache_image_count,
-            f_cache_image_offsets,
-        )
+        if fourcc != FourCC(FourCC::NONE) {
+            let mut header = DDSHeader::default();
+            header.width = width;
+            header.height = height;
+            header.spf.fourcc = Some(fourcc);
+
+            return Image::new(header, f_cache_image_count, f_cache_image_offsets);
+        } else {
+            let header =
+                DDSHeader::new_dxgi(height, width, None, format, None, None, None).unwrap();
+
+            return Image::new(header, f_cache_image_count, f_cache_image_offsets);
+        }
     }
 
     pub fn size(&self) -> usize {