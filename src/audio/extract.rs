d7fef3fa29beb3bdff5bec5d0af0cccdb2a7ddff
sehnryr <youn@melois.dev>
2023-03-23 16:19:10 +0100

    refactor: change too constraining audio header

--- src/audio/extract.rs
+++ src/audio/extract.rs
@@ -1,4 +1,4 @@
-use anyhow::{Ok, Result};
+use anyhow::{Error, Ok, Result};
 use log::debug;
 use lotus_lib::toc::node::Node;
 use lotus_lib::toc::FileNode;
@@ -7,7 +7,7 @@ use std::io::Write;
 use std::path::PathBuf;
 use std::rc::Rc;
 
-use crate::audio::header::ADPCMHeader;
+use crate::audio::header::{AudioCompressionFormat, AudioHeader};
 use crate::metadata::Metadata;
 use crate::shell::State;
 
@@ -20,7 +20,7 @@ pub fn extract(state: &State, file_node: Rc<RefCell<FileNode>>, output_dir: Path
     let metadata = Metadata::from(header_file_data.clone());
 
     // Create the audio
-    let header = ADPCMHeader::from_with_metadata(header_file_data, metadata)?;
+    let header = AudioHeader::from_with_metadata(header_file_data, metadata)?;
 
     debug!("Header: {:?}", header);
 
@@ -31,43 +31,30 @@ pub fn extract(state: &State, file_node: Rc<RefCell<FileNode>>, output_dir: Path
     let mut output_path = output_dir.clone();
     output_path.push(file_name);
 
-    // Get the file data
-    let b_cache = state.b_cache.unwrap();
-    let file_node = b_cache.get_file_node(file_node.path()).unwrap();
-    let _file_node = file_node.borrow();
+    if header.format_tag == AudioCompressionFormat::ADPCM {
+        output_path.set_extension("wav");
 
-    debug!("Cache offset: {}", _file_node.cache_offset() as u64);
-    debug!("Cache audio size: {}", _file_node.comp_len() as u64);
-    debug!("Real audio size: {}", header.size as u64);
-    debug!("Decompressed audio size: {}", _file_node.len() as u64);
+        // Get the file data
+        let b_cache = state.b_cache.unwrap();
+        let file_node = b_cache.get_file_node(file_node.path()).unwrap();
+        let _file_node = file_node.borrow();
 
-    let file_data = b_cache.decompress_data(file_node.clone())?;
-    let file_data = file_data[..header.size as usize].to_vec();
+        debug!("Cache offset: {}", _file_node.cache_offset() as u64);
+        debug!("Cache audio size: {}", _file_node.comp_len() as u64);
+        debug!("Real audio size: {}", header.size as u64);
+        debug!("Decompressed audio size: {}", _file_node.len() as u64);
 
-    // Write the file
-    let mut buffer = std::fs::File::create(output_path)?;
+        let file_data = b_cache.decompress_data(file_node.clone())?;
+        let file_data = file_data[..header.size as usize].to_vec();
 
-    buffer.write(b"RIFF")?;
-    buffer.write(&(header.size + 66).to_le_bytes())?;
-    buffer.write(b"WAVEfmt ")?;
-    buffer.write(&50u32.to_le_bytes())?;
-    buffer.write(&0x02u16.to_le_bytes())?;
-    buffer.write(&(header.channels as u16).to_le_bytes())?;
-    buffer.write(&header.samples_per_second.to_le_bytes())?;
-    buffer.write(&header.average_bytes_per_second.to_le_bytes())?;
-    buffer.write(&header.block_align.to_le_bytes())?;
-    buffer.write(&(header.bits_per_sample as u16).to_le_bytes())?;
-    buffer.write(&32u16.to_le_bytes())?;
-    buffer.write(&header.samples_per_block.to_le_bytes())?;
-    buffer.write(&(header.coefficients.len() as u16).to_le_bytes())?;
-    for coefficient in header.coefficients.iter() {
-        debug!("Coefficient: {:?}", coefficient);
-        buffer.write(&coefficient[0].to_le_bytes())?;
-        buffer.write(&coefficient[1].to_le_bytes())?;
+        // Write the file
+        let mut buffer = std::fs::File::create(output_path)?;
+
+        buffer.write_all(&header.to_wav_adpcm().unwrap())?;
+        buffer.write_all(&file_data)?;
+
+        return Ok(());
     }
-    buffer.write(b"data")?;
-    buffer.write(&header.size.to_le_bytes())?;
-    buffer.write_all(&file_data)?;
 
-    Ok(())
+    Err(Error::msg("Error extracting audio file"))
 }
