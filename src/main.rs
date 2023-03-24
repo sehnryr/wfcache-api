97732946f300b28f417fb41cdcd0aa71c11a2da0
sehnryr <youn@melois.dev>
2023-03-24 02:04:28 +0100

    feat: add basic completion

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,95 @@ mod shell;
 mod texture;
 mod utils;
 
+use std::cell::RefCell;
+use std::rc::Rc;
+
 use anyhow::Result;
 use clap::error::ErrorKind;
 use clap::{Parser, Subcommand};
 use log::{error, info};
 use lotus_lib::cache_pair::{CachePair, CachePairReader};
 use lotus_lib::package::{PackageCollection, PackageTrioType};
+use rustyline::completion::Completer;
+use rustyline::config::Configurer;
 use rustyline::error::ReadlineError;
-use rustyline::Editor;
+use rustyline::highlight::Highlighter;
+use rustyline::hint::Hinter;
+use rustyline::line_buffer::LineBuffer;
+use rustyline::validate::Validator;
+use rustyline::{CompletionType, Context, Editor, Helper};
 
+use crate::shell::command::ls::get_children;
 use crate::shell::command::{cd, find, get, ls, pwd, stat};
 use crate::shell::State;
+
+struct ApiHelper<'a> {
+    state: Rc<RefCell<State<'a>>>,
+}
+
+impl Validator for ApiHelper<'_> {}
+
+impl Highlighter for ApiHelper<'_> {}
+
+impl Hinter for ApiHelper<'_> {
+    type Hint = String;
+
+    fn hint(&self, line: &str, pos: usize, ctx: &Context<'_>) -> Option<Self::Hint> {
+        let _ = (line, pos, ctx);
+        None
+    }
+}
+
+impl Completer for ApiHelper<'_> {
+    type Candidate = String;
+
+    fn complete(
+        &self,
+        line: &str,
+        pos: usize,
+        _ctx: &rustyline::Context<'_>,
+    ) -> rustyline::Result<(usize, Vec<Self::Candidate>)> {
+        let mut candidates = Vec::new();
+
+        // Get the command
+        let command = line.split_whitespace().next();
+
+        // If there is no command, return an empty list
+        if command.is_none() || command.unwrap().len() == line.len() {
+            return Ok((0, Vec::with_capacity(0)));
+        }
+
+        // Get the last argument of the command
+        let last_arg_pos = line.trim_end().rfind(' ').unwrap_or(pos) + 1;
+        let last_arg = &line[last_arg_pos..pos];
+
+        // Get the current directory
+        let current_dir = self.state.borrow().current_lotus_dir.clone();
+
+        // Get the current directory node
+        let current_dir_node = self
+            .state
+            .borrow()
+            .h_cache
+            .get_directory_node(current_dir.to_str().unwrap())
+            .unwrap();
+
+        for node in get_children(current_dir_node) {
+            if node.1.starts_with(last_arg) {
+                candidates.push(node.1);
+            }
+        }
+
+        Ok((last_arg_pos, candidates))
+    }
+
+    fn update(&self, line: &mut LineBuffer, start: usize, elected: &str) {
+        let end = line.pos();
+        line.replace(start..end, elected);
+    }
+}
+
+impl Helper for ApiHelper<'_> {}
 
 /// The enum of sub-commands supported by the CLI
 #[derive(Subcommand, Clone, Debug)]
@@ -94,15 +172,20 @@ fn main() -> Result<()> {
     }
 
     // Initialize the state
-    let mut state = State::new(args.directory, h_cache.unwrap(), f_cache, b_cache);
+    let state = State::new(args.directory, h_cache.unwrap(), f_cache, b_cache);
+    let state = Rc::new(RefCell::new(state));
 
     // If the user specified a command, execute it and exit
     if let Some(command) = args.command {
-        parse_command(&mut state, command.as_str())?;
+        parse_command(&mut state.borrow_mut(), command.as_str())?;
         return Ok(());
     }
 
-    let mut rl = Editor::<()>::new()?;
+    let mut rl = Editor::<ApiHelper>::new()?;
+    rl.set_completion_type(CompletionType::List);
+    rl.set_helper(Some(ApiHelper {
+        state: state.clone(),
+    }));
     let prompt = "wfcache-api$ ";
 
     loop {
@@ -139,7 +222,7 @@ fn main() -> Result<()> {
         rl.add_history_entry(line);
 
         // Parse the command
-        parse_command(&mut state, line)?;
+        parse_command(&mut state.borrow_mut(), line)?;
 
         // Pad the output
         println!();
