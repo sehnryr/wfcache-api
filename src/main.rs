db2b98e58073f8c7076fb222ee34739dfbbcfee6
sehnryr <youn@melois.dev>
2024-03-30 23:57:57 +0100

    feat: :sparkles: add progress on app + add args back

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 mod app;
+mod args;
 mod errors;
 mod tui;
 mod widgets;
 
+use clap::Parser;
 use color_eyre::Result;
 
 fn main() -> Result<()> {
     errors::install_hooks()?;
+    let args = args::Args::parse();
     let mut terminal = tui::init()?;
-    app::App::default().run(&mut terminal)?;
+    app::App::try_init(args.directory, args.package, args.output)?.run(&mut terminal)?;
     tui::restore()?;
     Ok(())
 }
